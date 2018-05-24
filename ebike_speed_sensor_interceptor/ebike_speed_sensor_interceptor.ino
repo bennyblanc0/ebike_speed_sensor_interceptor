488c5dcf636e88d7ab4e1bb4e8795ae5aeaa3c16
Benjamin Sole <benjaminsole@gmail.com>
2018-05-24 18:53:08 +1200

    Initial upload

---
+++ ebike_speed_sensor_interceptor/ebike_speed_sensor_interceptor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ebike_speed_sensor_interceptor.ino
+ Created:	5/24/2018 6:09:59 PM
+ Author:	benja
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
